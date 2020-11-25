08955cfdd17d5047e0f212f16bc5c680ea71642f
megager <dimonzalis@gmail.com>
2020-11-25 23:43:18 +0200

    Delete case

--- tests/selenium-with-po.js
+++
@@ -1,9 +0,0 @@
-//var po = require('../pages/search_page.js');
-const page = po.searchPage;
-
-log.info("Running against " + page.url);
-
-page.open();
-page.search("Agile Testing");
-const visibleBooks = page.getNumberOfVisibleBooks();
-assert.equal(visibleBooks, 2, "There should only be one book visible");
