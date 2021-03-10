00edce5d5d66773e3aaf12862b052a0beea4890d
megager <dimonzalis@gmail.com>
2021-03-10 12:51:14 +0200

    Test

--- pages/search_page.js
+++ pages/search_page.js
@@ -2,7 +2,7 @@ module.exports = {
 
     searchPage: {
         url: 'https://automationbookstore.dev/',
-        searchBar: 'id=searchBar',
+        searchBar: 'id=searchBar1',
         hiddenBooks: 'li.ui-screen-hidden',
         visibleBooks: 'li:not(.ui-screen-hidden)',
 
